4b6f02b760e4129e488e9953494167e3f4030b1f
OD <kg-te@naver.com>
2022-12-16 20:29:00 +0900

    Time: 111 ms (55.14%), Space: 44.2 MB (69.18%) - LeetHub

--- 0268-missing-number/0268-missing-number.js
+++ 0268-missing-number/0268-missing-number.js
@@ -5,10 +5,8 @@
 var missingNumber = function(nums) {
     const sortedNums = Array.isArray(nums) && nums.sort((a,b)=> a-b);
     let min = 0;
-    
-    for(item of sortedNums) {
-    
-        if(item > min){
+    for(let i = 0; i <sortedNums.length; i++) {
+        if(sortedNums[i] > min){
             return min;
         }
         min += 1;
